4ba03c718c7bd209788883ebe23e865d9ce5aeaa
gothboiprncss <krystowskipiotr2005@gmail.com>
2024-10-16 21:44:27 +0200

    Added dwm config XD / Fixed border transparency issue.

--- fish/config.fish
+++ fish/config.fish
@@ -10,3 +10,6 @@ end
 function sudo
      command sudo $argv ;pokemon-colorscripts -n sudowoodo --no-title
 end
+alias dwm-config='cd ~/suckless/dwm && sudo nvim config.h && cd -'
+alias dwm-backup='cp ~/suckless/dwm/config.h ~/suckless/dwm/config.def.h && cp ~/suckless/dwm/config.def.h ~/suckless/dwm/backup'
+alias dwm-update='cd ~/suckless/dwm && sudo make clean install && cd -'
